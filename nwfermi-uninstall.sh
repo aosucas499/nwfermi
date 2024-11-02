f3f47a4ff8ff64fa66cb00d6b06b4a64f59b71e1
aosucas499 <aosucas499@gmail.com>
2024-11-02 22:27:24 +0100

    Actualizar nwfermi-uninstall.sh

--- nwfermi-uninstall.sh
+++ nwfermi-uninstall.sh
@@ -8,4 +8,6 @@ sudo apt-get purge --remove nwfermi-module-host -y
 sudo apt-get purge --remove nwfermi-module-host-pointer -y
 sudo apt-get purge --remove xf86-input-nextwindow -y
 
+dkms unbuild nwfermi/0.6.5.0 --all
+dkms unbuild nwfermi/0.7.0.1 --all
 
