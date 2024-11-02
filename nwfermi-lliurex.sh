64467d0b73df640b67e7bd42da7c7703e03a3324
aosucas499 <aosucas499@gmail.com>
2024-11-02 19:33:31 +0100

    new modules

--- nwfermi-lliurex.sh
+++ nwfermi-lliurex.sh
@@ -13,8 +13,8 @@ sudo apt-get purge --remove xf86-input-nextwindow -y
 # Los paquetes daemon, host y host-pointer no pueden estar instalados al mismo tiempo.
 
 sudo apt-get install libc6-i386 -y
-sudo dpkg -i ./lliurex21/nwfermi-daemon_0.6.5.1-0+lliurex7_amd64.deb
-sudo dpkg -i ./lliurex21/nwfermi-module_0.6.5.1-0+lliurex7_amd64.deb
-#sudo dpkg -i ./lliurex21/nwfermi-module-host_0.6.5.1-0+lliurex7_amd64.deb.deb
-#sudo dpkg -i ./lliurex21/nwfermi-module-host-pointer_0.6.5.1-0+lliurex7_amd64.deb.deb
+sudo dpkg -i ./lliurex21/nwfermi-daemon_0.6.5.1-0+lliurex11_amd64.deb
+sudo dpkg -i ./lliurex21/nwfermi-module_0.6.5.1-0+lliurex11_amd64.deb
+#sudo dpkg -i ./lliurex21/nwfermi-module-host_0.6.5.1-0+lliurex11_amd64.deb.deb
+#sudo dpkg -i ./lliurex21/nwfermi-module-host-pointer_0.6.5.1-0+lliurex11_amd64.deb.deb
 sudo dpkg -i ./lliurex21/xf86-input-nextwindow_0.3.4_lliurex1_amd64.deb
