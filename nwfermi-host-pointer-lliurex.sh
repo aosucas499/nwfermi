1c23833dc1159109aec6bafc14784f28c6d74480
aosucas499 <aosucas499@gmail.com>
2024-11-02 19:40:43 +0100

    fix last commit

--- nwfermi-host-pointer-lliurex.sh
+++ nwfermi-host-pointer-lliurex.sh
@@ -15,6 +15,6 @@ sudo apt-get purge --remove xf86-input-nextwindow -y
 sudo apt-get install libc6-i386 -y
 sudo dpkg -i ./lliurex21/nwfermi-daemon_0.6.5.1-0+lliurex11_amd64.deb
 #sudo dpkg -i ./lliurex21/nwfermi-module_0.6.5.1-0+lliurex11_amd64.deb
-#sudo dpkg -i ./lliurex21/nwfermi-module-host_0.6.5.1-0+lliurex11_amd64.deb.deb
-sudo dpkg -i ./lliurex21/nwfermi-module-host-pointer_0.6.5.1-0+lliurex11_amd64.deb.deb
+#sudo dpkg -i ./lliurex21/nwfermi-module-host_0.6.5.1-0+lliurex11_amd64.deb
+sudo dpkg -i ./lliurex21/nwfermi-module-host-pointer_0.6.5.1-0+lliurex11_amd64.deb
 sudo dpkg -i ./lliurex21/xf86-input-nextwindow_0.3.4_lliurex1_amd64.deb
